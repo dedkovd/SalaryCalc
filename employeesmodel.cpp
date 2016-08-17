58ec4e38daf3768ba5cb563aab4501aef7b6cf05
Denis V. Dedkov <denis.v.dedkov@gmail.com>
2016-08-17 23:39:36 +0500

    EmployeesModel was added

---
+++ employeesmodel.cpp
@@ -0,0 +1,7 @@
+#include "employeesmodel.h"
+
+EmployeesModel::EmployeesModel()
+{
+
+}
+
---
+++ employeesmodel.h
@@ -0,0 +1,12 @@
+#ifndef EMPLOYEESMODEL_H
+#define EMPLOYEESMODEL_H
+
+#include <QObject>
+
+class EmployeesModel : public QAbstractItemModel
+{
+public:
+    EmployeesModel();
+};
+
+#endif // EMPLOYEESMODEL_H
